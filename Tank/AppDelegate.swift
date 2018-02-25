8576bb0862581fdb569a00744a4add30938161a6
Xavier Bauquet <xavier.bauquet@gmail.com>
2018-02-25 11:24:47 +0100

    INITIAL commit

---
+++ Tank/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tank
+//
+//  Created by Work on 25/02/2018.
+//  Copyright © 2018 xbauquet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tank/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tank
+//
+//  Created by Work on 25/02/2018.
+//  Copyright © 2018 xbauquet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
