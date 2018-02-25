5170c2f96a11ef22a8b90a83cd89d50e3b550cb4
Xavier Bauquet <xavier.bauquet@gmail.com>
2018-02-25 15:53:01 +0100

    ADD main window

--- Tank/ViewController.swift
+++ Tank/ViewController.swift
@@ -21,6 +21,16 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    static func get() -> ViewController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let identifier = NSStoryboard.SceneIdentifier("ViewController")
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? ViewController else {
+            fatalError("Why cant i find LoginViewController? - Check Main.storyboard")
+            // TODO
+        }
+        return viewcontroller
+    }
 
 
 }
